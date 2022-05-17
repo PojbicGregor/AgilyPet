95af7948abebaaf49c1dde8d7f1e63c4d0df2232
blazhe_manev <manev.blazhe@student.um.si>
2022-05-17 12:31:59 +0200

    Register/login Authentication(JWT) konec

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,8 @@ const jwt = require('jsonwebtoken')
 
 const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
 
+const { stringify } = require('querystring');
+const fetch = require('node-fetch');
 const port = 3001;
 const app = express();
 const MONGODB_URI = 'mongodb+srv://AgilyPet:PWFp2JX63wJkfAc@agilypet.8wt9o.mongodb.net/agilyPet?retryWrites=true&w=majority';
@@ -64,7 +66,7 @@ function runsrvr () {
 const bodyParser= require('express');
 app.use(bodyParser.json());
 app.get("/register", (req, res) => {
-    res.sendFile(__dirname + "/views/doma.html");
+    res.sendFile(__dirname + "/views/register.html");
     app.use(bodyParser.json());
 
   });
@@ -76,11 +78,14 @@ app.get("/register", (req, res) => {
 
 
   app.post('/api/register', async(req,res)=>{
-    const {username,password:plainTextPassword}=req.body;
+    const {email,username,password:plainTextPassword}=req.body;
     const password = await bcrypt.hash(plainTextPassword, 10)
 
    	if (!username || typeof username !== 'string') {
 		return res.json({ status: 'error', error: 'Invalid username' })
+	}
+	if (!email || typeof email !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid email' })
 	}
 
 	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
@@ -96,6 +101,7 @@ app.get("/register", (req, res) => {
 
 	try {
 		const response = await Uporabnik.create({
+			email,
 			username,
 			password
 		})
@@ -103,7 +109,7 @@ app.get("/register", (req, res) => {
 	} catch (error) {
 		if (error.code === 11000) {
 			// duplicate key
-			return res.json({ status: 'error', error: 'Username already in use' })
+			return res.json({ status: 'error', error: 'Username/email already in use' })
 		}
 		throw error
 	}
@@ -114,8 +120,8 @@ app.get("/register", (req, res) => {
 
   app.post('/api/login', async (req,res)=>{
     
-    const { username, password } = req.body
-	const user = await Uporabnik.findOne({ username }).lean()
+    const { email, password } = req.body
+	const user = await Uporabnik.findOne({ email }).lean()
 
 	if (!user) {
 		return res.json({ status: 'error', error: 'Invalid username/password' })
@@ -127,11 +133,18 @@ app.get("/register", (req, res) => {
 		const token = jwt.sign(
 			{
 				id: user._id,
-				username: user.username
+				email: user.email
 			},
 			JWT_SECRET
 		)
+		const _id=user._id;
 
+       await Uporabnik.updateOne(
+		   {_id},
+		   {
+			   $set:{tokeni:token}
+		   }
+	   )
 		return res.json({ status: 'ok', data: token })
 	}
 
--- backend/schemas/uporabnik.js
+++ backend/schemas/uporabnik.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs")
 const uporabnikShema = new mongoose.Schema({
+    email: {type:String ,required:true, unique:true},
     username: {type:String ,required:true, unique:true},
-    password: {type:String ,required:true}
+    password: {type:String ,required:true},
+    tokeni:{type:String}
 });
 
 module.exports = mongoose.model("Registriran_Uporabnik", uporabnikShema);
