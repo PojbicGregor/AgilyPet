8bf2b969b54c7df848a8934e0c35eeac52f09ce6
LukacJan <jan.lukac@student.um.si>
2022-05-25 17:11:15 +0200

    Popravek

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require('body-parser');
 const cors = require("cors");
 const {google} = require('googleapis');
 const bodyParser = require('body-parser'); // tuka
 
-var cors = require('cors');
+
 var allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
 const app = express();
 app.use(cors({
