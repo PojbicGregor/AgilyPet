79e2edb838f96aa75dcea6cf78a88e4bd99c9d88
blazhe_manev <manev.blazhe@student.um.si>
2022-05-28 18:23:12 +0200

    priprava na dodajanje dogodkov(events)

---
+++ backend/controllers/eventController.js
@@ -0,0 +1,128 @@
+var EventModel = require('../models/eventModel.js');
+
+/**
+ * eventController.js
+ *
+ * @description :: Server-side logic for managing events.
+ */
+module.exports = {
+
+    /**
+     * eventController.list()
+     */
+    list: function (req, res) {
+        EventModel.find(function (err, events) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Error when getting event.',
+                    error: err
+                });
+            }
+
+            return res.json(events);
+        });
+    },
+
+    /**
+     * eventController.show()
+     */
+    show: function (req, res) {
+        var id = req.params.id;
+
+        EventModel.findOne({_id: id}, function (err, event) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Error when getting event.',
+                    error: err
+                });
+            }
+
+            if (!event) {
+                return res.status(404).json({
+                    message: 'No such event'
+                });
+            }
+
+            return res.json(event);
+        });
+    },
+
+    /**
+     * eventController.create()
+     */
+    create: function (req, res) {
+        var event = new EventModel({
+			ime : req.body.ime,
+			datum : req.body.datum,
+			opis : req.body.opis,
+			naslov : req.body.naslov
+        });
+
+        event.save(function (err, event) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Error when creating event',
+                    error: err
+                });
+            }
+
+            return res.status(201).json(event);
+        });
+    },
+
+    /**
+     * eventController.update()
+     */
+    update: function (req, res) {
+        var id = req.params.id;
+
+        EventModel.findOne({_id: id}, function (err, event) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Error when getting event',
+                    error: err
+                });
+            }
+
+            if (!event) {
+                return res.status(404).json({
+                    message: 'No such event'
+                });
+            }
+
+            event.ime = req.body.ime ? req.body.ime : event.ime;
+			event.datum = req.body.datum ? req.body.datum : event.datum;
+			event.opis = req.body.opis ? req.body.opis : event.opis;
+			event.naslov = req.body.naslov ? req.body.naslov : event.naslov;
+			
+            event.save(function (err, event) {
+                if (err) {
+                    return res.status(500).json({
+                        message: 'Error when updating event.',
+                        error: err
+                    });
+                }
+
+                return res.json(event);
+            });
+        });
+    },
+
+    /**
+     * eventController.remove()
+     */
+    remove: function (req, res) {
+        var id = req.params.id;
+
+        EventModel.findByIdAndRemove(id, function (err, event) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Error when deleting the event.',
+                    error: err
+                });
+            }
+
+            return res.status(204).json();
+        });
+    }
+};
---
+++ backend/models/eventModel.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+module.exports = mongoose => {
+
+var eventSchema = new Schema({
+	ime: { type: String, required: true },
+	datum: { type: Number, required: true },
+	opis: {type: String, required: true},
+	naslov: {type: String, required:true}
+});
+
+eventSchema.method("toJSON", function() {
+	const { __v, _id, ...object } = this.toObject();
+	object.id = _id;
+	return object;
+  });
+
+  const Event = mongoose.model("events", eventSchema);
+  return Event;
+}
