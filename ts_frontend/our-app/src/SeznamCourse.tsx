4c448e2d338001e390940237f60854cc9cd617f8
PojbicGregor <gregor.pojbic@student.um.si>
2022-06-05 21:39:13 +0200

    follower sistem

--- ts_frontend/our-app/src/SeznamCourse.tsx
+++ ts_frontend/our-app/src/SeznamCourse.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { MouseEventHandler, useRef } from 'react';
 import { ChangeEvent } from 'react';
 import { FormEvent } from 'react';
 import { Col, Row, Card, Container, Badge } from 'react-bootstrap';
@@ -7,6 +7,8 @@ import Velikost from './Velikost';
 import ZdrastvenoStanje from './ZdrastvenoStanje';
 
 const SeznamCourse: React.FC = () => {
+
+    const ref = useRef(null);
 
     const [elements, setElements] = React.useState<Course[]>();
 
@@ -17,8 +19,9 @@ const SeznamCourse: React.FC = () => {
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
 
+        console.log(ref.current);
         let podatki = {
-            email: (document.getElementById("dodal") as HTMLInputElement).value,
+            email: ref.current,
             token: localStorage.getItem("token")
         }
 
@@ -45,6 +48,30 @@ const SeznamCourse: React.FC = () => {
 
     const handleChange2 = (e: ChangeEvent<HTMLInputElement>) => {
         setLastnosti({ ...lastnosti, [e.target.name]: e.target.value });
+    }
+
+    const handleClick = (s : string) => {
+        
+        let podatki = {
+            email: s,
+            token: localStorage.getItem("token")
+        }
+
+        fetch("http://localhost:3001/api/follow", {
+            method: 'POST',
+            body: JSON.stringify(podatki),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            if (response.status === 200) {
+                console.log(response);
+                response.json().then(podatki => {
+                    console.log(localStorage.getItem("token"));
+                    localStorage.getItem("token");              
+                })
+            }
+        })
     }
 
 
@@ -122,6 +149,19 @@ const SeznamCourse: React.FC = () => {
                                 {JSON.stringify(course.sklepi) === 'true' ? <><span><Badge bg="warning" text="dark">Joint issues</Badge></span><br /></> : null}
                                 {(JSON.stringify(course.manjkaEna) === 'true' || JSON.stringify(course.manjkataDve) === 'true' || JSON.stringify(course.sklepi) === 'true') ? <><span><Badge bg="success">No issues</Badge></span><br /></> : null}
                                 </Card.Text>
+                                {/*<form id="form" onSubmit={handleSubmit}>
+
+                                    <select name="jeDodal" value={lastnosti.jeDodal} onChange={handleChange}>
+                                        <option  value={course.jeDodal}>{course.jeDodal}</option>
+                                        <option  value="dingdong@gmail.com">test</option>
+                                    </select>
+                                    <input ref={ref} name="dodal" type="text" value={course.jeDodal} ></input>
+                                    
+
+                                    <input type="submit" value="Follow"/>
+                                </form>*/}
+
+                                <button onClick= {() => handleClick(course.jeDodal)}>Follow</button>
                             </Card.Body>
                             {(course.jeDodal !== undefined && (course.jeDodal.includes('admin') || course.jeDodal.includes('Admin')) && !course.jeDodal.includes('@')) ? 
                             <Card.Footer className='small_foot_min'>
