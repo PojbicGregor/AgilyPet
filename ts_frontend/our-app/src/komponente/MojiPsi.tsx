4f78e536e1482b9b440d4ee0202f3ccc2af8cb31
blazhe_manev <manev.blazhe@student.um.si>
2022-06-05 12:09:10 +0200

    prikazovanje Moji Psi in izbris psa(konec)

--- ts_frontend/our-app/src/komponente/MojiPsi.tsx
+++ ts_frontend/our-app/src/komponente/MojiPsi.tsx
@@ -6,14 +6,15 @@ import UserNav from './UserNav';
 import { MouseEvent } from 'react';
 const MojiPsi: React.FC = () => {
     const [elements, setElements] = React.useState<Pes[]>();
-   
+    let preveri;
+
 
 
     React.useEffect(function () {
         const getPsi = async function () {
             const res = await fetch("http://localhost:3001/token/" + localStorage.getItem("token"));
             const data = await res.json();
-            setElements(data); 
+            setElements(data);
         }
         getPsi();
     }, [])
@@ -31,48 +32,59 @@ const MojiPsi: React.FC = () => {
                 })
             }
         })
-        
+
     }
-    const  handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+    const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         let dataForBackend = {
             id: e.currentTarget.id,
             token: localStorage.getItem("token"),
         }
-       deleteDog(dataForBackend)
+        deleteDog(dataForBackend)
     }
-    return (
+    if (elements === undefined || elements.length == 0) {
+        return (<>            <UserNav />
+            <h1 className="text-center">
+                Dear, you have not added a Dog yet, click on the button to add a Dog</h1>
+            <Button href='/dodajPsa'>ADD</Button>
+            <Noga />
+        </>)
+    }
+    else {
+        return (
 
-        <><UserNav />
+            <><UserNav />
+                    <h1 className="text-center">Dogs added by me</h1>
 
-            <div className='container' style={{ backgroundColor: "white", borderRadius: "15px" }}>
+                <div className='container' style={{ backgroundColor: "white", borderRadius: "15px" }}>
 
-                <div className='container-md' >
+                    <div className='container-md' >
 
-                    {elements?.map(psi => (<div style={{ border: "solid 4px whiteSmoke", borderRadius: "10px", margin: "15px" }} key={psi.ime}>
+                        {elements?.map(psi => (<div style={{ border: "solid 4px whiteSmoke", borderRadius: "10px", margin: "15px" }} key={psi.ime}>
 
-                        <span><h3>{psi.ime}</h3></span><br />
+                            <span><h3>{psi.ime}</h3></span><br />
 
-                        <span>{psi.pasma}</span><br />
+                            <span>{psi.pasma}</span><br />
 
-                        <span>{psi.visina}</span><br />
+                            <span>{psi.visina}</span><br />
 
-                        <span>{psi.starost}</span><br />
-                        <span>Missing a limb:{JSON.stringify(psi.manjkaEna)}</span><br />
-                        <span>Missing two limbs:{JSON.stringify(psi.manjkataDve)}</span><br />
-                        <span>Dog with joint issues:{JSON.stringify(psi.sklepi)}</span><br />
-                        <Button variant='danger' id={psi._id.toString()} onClick={handleClick}>Delete</Button>
-                    </div>))}
+                            <span>{psi.starost}</span><br />
+                            <span>Missing a limb:{JSON.stringify(psi.manjkaEna)}</span><br />
+                            <span>Missing two limbs:{JSON.stringify(psi.manjkataDve)}</span><br />
+                            <span>Dog with joint issues:{JSON.stringify(psi.sklepi)}</span><br />
+                            <Button variant='danger' id={psi._id.toString()} onClick={handleClick}>Delete</Button>
+                        </div>))}
+
+                    </div>
 
                 </div>
 
-            </div>
-
-            <Noga />
-        </>
+                <Noga />
+            </>
 
 
 
-    )
+        )
+    }
 }
 export default MojiPsi;
